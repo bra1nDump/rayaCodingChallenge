8d1047b69c6e349f95ab0509b56d2ce641be86c7
kostas-mac-client <kostas2003de@icloud.com>
2020-01-19 01:00:21 -0800

    Initial Commit

---
+++ RayaChallangeTests/RayaChallangeTests.swift
@@ -0,0 +1,34 @@
+//
+//  RayaChallangeTests.swift
+//  RayaChallangeTests
+//
+//  Created by Konstantin Dubovitskiy on 1/19/20.
+//  Copyright © 2020 Kirill Dubovitskiy. All rights reserved.
+//
+
+import XCTest
+@testable import RayaChallange
+
+class RayaChallangeTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
